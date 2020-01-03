1951d0e872278f409b0fd18d2cba02eb630aefcf
Ronald Painter <56695551+ronpaintertech@users.noreply.github.com>
2020-01-02 21:51:28 -0500

    Add files via upload

---
+++ portfolio/CCPSTopSocialMediaNetworks/story_content/5s43KPemMwB_44100_48_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.35.21017.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.075%0D%0ANow%20let%E2%80%99s%20look%20at%20YouTube.%20%20Click%20on%20the%20YouTube%20logo%20to%20start.%0D%0A%0D%0A"}')
---
+++ portfolio/CCPSTopSocialMediaNetworks/story_content/6Wu7ILvLoBo_44100_48_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.35.21017.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.291%0D%0AClick%20on%20each%20box%20to%20reveal%20a%20popular%20feature%20of%20YouTube.%0D%0A%0D%0A"}')
